e39f7309f58983668cf1ae21e40bdb3cc3e847ce
Mooner92 <54096383+mooner92@users.noreply.github.com>
2023-10-15 21:25:03 +0900

    Initial commit

---
+++ src/main/java/com/mooner92/obok_backend/ObokBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mooner92.obok_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObokBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObokBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mooner92/obok_backend/ObokBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mooner92.obok_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObokBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
